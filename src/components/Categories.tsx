89d6177472988ac0febd09aab766d63e0de5bf47
unknown <tiimwqq@gmail.com>
2024-12-02 13:33:39 +0500

    refactor: add TypeScript typing to Categories component

---
+++ src/components/Categories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+type CategoriesProps = {
+	categoryId: number;
+	setCategoryId: (id: number) => void
+}
+
+const Categories: React.FC<CategoriesProps> = ({ categoryId, setCategoryId }) => {
+
+	const categories: string[] = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+
+	return (
+		<div className="categories">
+			<ul>
+				{
+					categories.map((data, index) => (
+						<li key={index}
+							onClick={() => setCategoryId(index)}
+							className={categoryId === index ? 'active' : ''}
+						>{data}</li>
+					))
+				}
+			</ul>
+		</div >
+	);
+}
+
+export default Categories;
