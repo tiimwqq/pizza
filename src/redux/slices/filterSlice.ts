578068ed5a4720509ff67b5894811323c021e858
unknown <tiimwqq@gmail.com>
2024-12-24 17:54:36 +0500

    refactor: add ts typing to filterSlice.ts component

---
+++ src/redux/slices/filterSlice.ts
@@ -0,0 +1,61 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export type SortProps = {
+	name: string;
+	sortProperty: 'rating' | 'rating asc' | 'price' | 'price asc' | 'title' | 'title asc';
+	value: 'desc' | 'asc'
+}
+
+export interface IfilterSlice {
+	searchValue: string;
+	categoryId: number;
+	sort: SortProps;
+}
+
+const initialState: IfilterSlice = {
+	searchValue: '',
+	categoryId: 0,
+	sort: { name: 'популярности ↓', sortProperty: 'rating', value: 'desc' },
+}
+
+export const filterSlice = createSlice({
+	name: 'filters',
+	initialState,
+	reducers: {
+		setCategoryId(state, action: PayloadAction<number>) {
+			state.categoryId = action.payload
+		},
+		setSearchValue(state, action: PayloadAction<string>) {
+			state.searchValue = action.payload
+		},
+		setSort(state, action: PayloadAction<SortProps>) {
+			state.sort = action.payload
+		},
+		setFilter(state, action: PayloadAction<IfilterSlice>) {
+
+			const { searchValue, categoryId, sort } = action.payload;
+
+			if (searchValue !== undefined) {
+				state.searchValue = searchValue;
+			}
+
+			if (categoryId !== undefined) {
+				state.categoryId = categoryId;
+			}
+
+			if (sort) {
+				state.sort = {
+				  ...state.sort,
+				  ...sort,
+				};
+			  } else {
+				state.sort = { name: 'популярности ↓', sortProperty: 'rating', value: 'desc' };
+			  }
+		}
+	},
+})
+
+export const { setCategoryId, setSort, setFilter, setSearchValue } = filterSlice.actions
+
+export default filterSlice.reducer
+
